ab80555d306139892a32b9e6bac4444037a53009
hawkchen <hawkchen@potix.com>
2020-02-10 15:49:25 +0800

    scroll sidebar upon the current page with requestAnimationFrame()

--- assets/js/page-control.js
+++ assets/js/page-control.js
@@ -1,5 +1,12 @@
-window.onload = function(){
-    //scroll side bar to the current visited page
-    var currentPageOffsetTop = document.querySelector("a[href=\"" + window.location.pathname + "\"]").offsetTop;
-    document.querySelector('.sidebar').scrollTop = currentPageOffsetTop;
-}
+var isSidebarReady = function () {
+	if (document.body && document.querySelector('.sidebar')) {
+        //scroll side bar to the current visited page
+        var currentPageOffsetTop = document.querySelector("a[href=\"" + window.location.pathname + "\"]").offsetTop;
+        document.querySelector('.sidebar').scrollTop = currentPageOffsetTop;
+		return;
+	}
+
+	window.requestAnimationFrame(isSidebarReady);
+};
+// IE10 or above
+window.requestAnimationFrame(isSidebarReady);
