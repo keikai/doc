13a94514069ba6e773243ee84e928c8350c0a723
hawkchen <hawkchen@potix.com>
2020-03-03 11:33:29 +0800

    scroll the selected title with 20px margin

--- assets/js/page-control.js
+++ assets/js/page-control.js
@@ -1,9 +1,11 @@
 var isSidebarReady = function () {
-	if (document.body && document.querySelector('.sidebar')) {
+	if (document.body //detect the body is loaded
+		&& document.querySelector('.sidebar')) {
 		//scroll side bar to the current visited page
 		var elCurrentAnchor = document.querySelector("a[href=\"" + window.location.pathname + "\"]");
         var currentPageOffsetTop = elCurrentAnchor.offsetTop;
-		document.querySelector('.sidebar').scrollTop = currentPageOffsetTop;
+		document.querySelector('.sidebar').scrollTop = currentPageOffsetTop - 20; //20px is a margin space
+
 		//mark the current page CSS class
 		elCurrentAnchor.classList.add('current');
 		return;
