0e4172f546593183748fea0a63eaf077fce8a52f
mduchemin <matthieu@potix.com>
2025-11-03 18:39:44 +0100

    support cookie-management and GA4 loader

--- assets/js/page-control.js
+++ assets/js/page-control.js
@@ -3,11 +3,13 @@ var isSidebarReady = function () {
 		&& document.querySelector('.sidebar')) {
 		//scroll side bar to the current visited page
 		var elCurrentAnchor = document.querySelector("a[href=\"" + window.location.pathname + "\"]");
-        var currentPageOffsetTop = elCurrentAnchor.offsetTop;
-		document.querySelector('.sidebar').scrollTop = currentPageOffsetTop - 20; //20px is a margin space
-
-		//mark the current page CSS class
-		elCurrentAnchor.classList.add('current');
+		if(elCurrentAnchor){
+			var currentPageOffsetTop = elCurrentAnchor.offsetTop;
+			document.querySelector('.sidebar').scrollTop = currentPageOffsetTop - 20; //20px is a margin space
+			
+			//mark the current page CSS class
+			elCurrentAnchor.classList.add('current');
+		}
 		return;
 	}
 
